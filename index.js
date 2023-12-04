e02d5c660e25b84feb1833de066be80d647f45fe
delwarHosen <delwar.hosen008@gmail.com>
2023-12-03 23:38:25 -0800

    change something

--- index.js
+++ index.js
@@ -5,18 +5,45 @@ const port = process.env.PORT || 5000;
 const cors = require('cors')
 app.use(cors())
 
+// data loded
 const categories = require('./data/categories.json')
+const courses = require('./data/courses.json')
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('code script is running');
 })
 
 // loaded category data
-app.get('/categories',(req,res)=>{
+app.get('/categories', (req, res) => {
     res.send(categories)
 })
 
-app.listen(port, (req,res)=>{
+// All courses data loaded
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+// specific course loaded by category id
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id
+    if (id === '0') {
+        res.send(courses)
+    } else {
+        const selectedCategory = courses.filter(n => n.category_id === id)
+        res.send(selectedCategory)
+    }
+})
+
+// All courses load with specific id
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(n => n._id === id)
+    res.send(selectedCourse)
+})
+
+
+
+app.listen(port, (req, res) => {
     console.log(`Code server running on : ${port}`);
 })
